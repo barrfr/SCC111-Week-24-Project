615fa911c2c4b4cbbe489c619df8434b077d2b78
BT <90915136+barrfr@users.noreply.github.com>
2026-05-08 16:23:47 +0100

    Update GUI.java task 3 FINAL
    commented code according to task 3

--- GUI.java
+++ GUI.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GUI extends JFrame {
+
+    //declaring constants
     private final int ROWS = 4;
     private final int COLS = 5;
     private final int TILE_SIZE = 100;
@@ -9,24 +11,29 @@ public class GUI extends JFrame {
     private Model model;
     private Controller controller;
 
+    //setting up images and game panel
     private JPanel boardPanel;
-
     private ImageIcon emptyIcon;
     private ImageIcon treeIcon;
     private ImageIcon BheadIcon;
+    private ImageIcon RheadIcon;
+    private ImageIcon YheadIcon;
     private ImageIcon bodyIcon;
     private ImageIcon middleIcon;
+    private ImageIcon stackIcon;
+    private ImageIcon RmanIcon;
     private ImageIcon BmanIcon;
+    private ImageIcon YmanIcon;
 
     public GUI(Model model){
         this.model = model;
         
+        //instantiate panel and set up game space
         setTitle("Snowman Puzzle Game");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
 
         loadImages();
-
         boardPanel = new JPanel();
         boardPanel.setLayout(new GridLayout(ROWS, COLS));
 
@@ -34,26 +41,34 @@ public class GUI extends JFrame {
 
         renderBoard(this.model.getBoard());
 
+        //auto adjusts size and location of panel to fit board & be centre screen
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
     }
 
+    //kickstart user input
     public void setController(Controller controller){
         this.controller = controller;
     }
 
+    //made a function to reduce clutter in constructor
     private void loadImages(){
         emptyIcon = resizeIcon("hole.png");
         treeIcon = resizeIcon("tree.png");
         BheadIcon = resizeIcon("head_blue.png");
+        RheadIcon = resizeIcon("head_red.png");
+        YheadIcon = resizeIcon("head_yellow.png");
         bodyIcon = resizeIcon("snowball_large.png");
         middleIcon = resizeIcon("snowball_small.png");
+        stackIcon = resizeIcon("snowman_stack.png");
+        RmanIcon = resizeIcon("snowman_red.png");
         BmanIcon = resizeIcon("snowman_blue.png");
-
+        YmanIcon = resizeIcon("snowman_yellow.png");
 
     }
 
+    //make icon fit the game board
     private ImageIcon resizeIcon(String filename){
         ImageIcon originalIcon = new ImageIcon(filename);
 
@@ -62,17 +77,15 @@ public class GUI extends JFrame {
         return new ImageIcon(scaledImage);
     }
 
-    public void renderGameFinish(boolean game_won){
-
-    }
-
     public void renderBoard(String[][] board){
+        //reset board render
         boardPanel.removeAll();
 
+        //iterate through the model board, & if detecting a movable piece, create an interactable button
         for (int row = 0; row < ROWS; row++){
             for (int col = 0; col < COLS; col++){
-
                 String tile = board[row][col];
+
                 if(tile.equals("b")){
                     JButton button = createTileButton(bodyIcon, row, col);
                     boardPanel.add(button);
@@ -83,6 +96,12 @@ public class GUI extends JFrame {
                     boardPanel.add(button);
                 }
 
+                else if(tile.equals("s")){
+                    JButton button = createTileButton(stackIcon, row, col);
+                    boardPanel.add(button);
+                }
+
+                //if no movable piece, create a label
                 else{
                     JButton label = createTileButton(getIconForTile(tile), row, col);
                     boardPanel.add(label);
@@ -95,14 +114,17 @@ public class GUI extends JFrame {
         System.out.println("Board Rendered");
     }
 
+
     private JButton createTileButton(ImageIcon icon, int row, int col){
         JButton button = new JButton(icon);
 
+        //adjusts button parameters
         button.setPreferredSize(new Dimension(TILE_SIZE, TILE_SIZE));
         button.setBorderPainted(false);
         button.setContentAreaFilled(false);
         button.setFocusPainted(false);
 
+        //detects when the button is clicked and calls onTileClicked()
         button.addActionListener(e -> {
             onTileClicked(row, col);
         });
@@ -110,6 +132,7 @@ public class GUI extends JFrame {
         return button;
     }
 
+    //used to detect what non-interactable object is currently trying to be rendered
     private ImageIcon getIconForTile(String tile){
         if(tile.equals("t")){
             return treeIcon;
@@ -119,8 +142,24 @@ public class GUI extends JFrame {
             return BheadIcon;
         }
 
+        if(tile.equals("HR")){
+            return RheadIcon;
+        }
+
+        if(tile.equals("HY")){
+            return YheadIcon;
+        }
+
+        if(tile.equals("SR")){
+            return RmanIcon;
+        }
+
         if(tile.equals("SB")){
             return BmanIcon;
+        }
+
+        if(tile.equals("SY")){
+            return YmanIcon;
         }
 
         return emptyIcon;
