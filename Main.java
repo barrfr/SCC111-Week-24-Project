51ef6dfd04fc44eafb7f2161268bf10c5642951e
BT <90915136+barrfr@users.noreply.github.com>
2026-05-08 16:21:47 +0100

    Update Main.java task 3
    main according to task 3

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args){
         
+        //instantiate appropriate classes & feed them into each other
         Model model = new Model();
         GUI gui = new GUI(model);
         Controller controller = new Controller(model, gui);
 
+        //begin listening for user input
         gui.setController(controller);
 
     }
